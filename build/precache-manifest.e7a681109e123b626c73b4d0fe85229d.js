cea7b7dd9fb2d4a9931183de28d199f899761066
Jose Andres <vitodua07@gmail.com>
2020-06-18 17:59:07 -0600

    Proyecto-Individual Jose Andres Duarte Hidalgo B82639

---
+++ build/precache-manifest.e7a681109e123b626c73b4d0fe85229d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff8448116bb550ebabe2f2cd1c9a40d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d750e29657b5b0cc22ce",
+    "url": "./static/css/main.a62359dd.chunk.css"
+  },
+  {
+    "revision": "c6e0cd65986cfc9b5753",
+    "url": "./static/js/2.d21597a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d21597a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d750e29657b5b0cc22ce",
+    "url": "./static/js/main.ce04904b.chunk.js"
+  },
+  {
+    "revision": "7f870a9931d72cdb3a4e",
+    "url": "./static/js/runtime-main.9526db45.js"
+  },
+  {
+    "revision": "40888dfac56677d1d58e12cf2443b1ed",
+    "url": "./static/media/1.40888dfa.JPG"
+  },
+  {
+    "revision": "57acb96f7756fef42ddc78f798d0b905",
+    "url": "./static/media/1.57acb96f.JPG"
+  },
+  {
+    "revision": "1667a0fbb59af54702c87a20a7462d95",
+    "url": "./static/media/2.1667a0fb.gif"
+  },
+  {
+    "revision": "7b39edc7ad8ddce415b2a11161cf8a6a",
+    "url": "./static/media/3.7b39edc7.gif"
+  },
+  {
+    "revision": "e3fb55a26221e62cb861852138a46274",
+    "url": "./static/media/4.e3fb55a2.gif"
+  },
+  {
+    "revision": "08958e6ec57a6f4018b1f4be4edc29a8",
+    "url": "./static/media/5.08958e6e.png"
+  },
+  {
+    "revision": "dadc2da9ab2585c6ac0be12e1a8a0bbb",
+    "url": "./static/media/about3.dadc2da9.jpeg"
+  }
+]);
